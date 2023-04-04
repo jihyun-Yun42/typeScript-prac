2e3f38cee6cd61de44c0a9aac8d60c603ffc16bc
jihyun Yun <jyun9842@gmail.com>
2023-04-04 11:10:53 +0900

    [feat] todolist ver1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { JsxElement } from 'typescript';
 
-function App() {
+function App(): JSX.Element {
+  const [addTodo, setAddTodo] = useState({
+    title: '',
+    content: '',
+  });
+
+  const [todo, setTodo] = useState([
+    { title: '타스뽀개기', content: '3단계까지 마무으리' },
+  ]);
+  const changeInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    const { name, value } = e.target;
+    setAddTodo({ ...addTodo, [name]: value });
+  };
+
+  const submitFormHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodo([...todo, addTodo]);
+    setAddTodo({ title: '', content: '' });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>TodoList</h1>
+      <form onSubmit={submitFormHandler}>
+        <input
+          type="text"
+          name="title"
+          value={addTodo.title}
+          onChange={changeInputHandler}
+        />
+        <input
+          type="text"
+          name="content"
+          value={addTodo.content}
+          onChange={changeInputHandler}
+        />
+        <button type="submit">추가</button>
+      </form>
+      {todo.map((item) => (
+        <div style={{ width: '100px', height: '100px', border: '1px solid' }}>
+          <p>{item.title}</p>
+          <p>{item.content}</p>
+        </div>
+      ))}
+    </>
   );
 }
 
